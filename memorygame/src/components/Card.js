4bdb057e67e3b3975c5f8a17fa59e026815e86ec
Noman Ashraf <nomanashraf874@gmail.com>
2023-03-27 14:41:14 -0400

    fixing pull request error

---
+++ memorygame/src/components/Card.js
@@ -0,0 +1,25 @@
+import "./Card.css";
+
+const Card = ({ card, handleChoice, flipped, disabled }) => {
+    const handleClick = () => {
+      if (!disabled) {
+        handleChoice(card);
+      }
+    };
+  
+    return (
+      <div className="card">
+        <div className={flipped ? "flipped" : ""}>
+          <img className="front" src={card.src} alt="card-front" />
+          <img
+            className="back"
+            src="poke.png"
+            alt="card-back"
+            onClick={handleClick}
+          />
+        </div>
+      </div>
+    );
+  };
+  
+  export default Card;
